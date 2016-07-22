b8d0abf08a47d0e0d791a972e30d4c98b35eef9f
michael-conway <michael_conway@unc.edu>
2016-07-22 16:43:41 -0400

    #1 playing with base socket pair

---
+++ zeromq-provider/src/main/java/org/irods/jargon2/provider/zeromq/sandbox/package-info.java
@@ -0,0 +1,7 @@
+
+/**
+ * throw away sandbox to start playing with zeromq
+ * @author mconway
+ *
+ */
+package org.irods.jargon2.provider.zeromq.sandbox;
---
+++ zeromq-server-emulator/src/main/java/org/irods/jargon2/provider/zeromq/sandbox/server/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Play sandbox for server side of zeromq
+ * @author mconway
+ *
+ */
+package org.irods.jargon2.provider.zeromq.sandbox.server;
