cf79ef49409a5dc51df47c8edca0d5de88b65f6e
michael-conway <michael_conway@unc.edu>
2017-03-21 16:31:05 -0400

    #1 added base packages

---
+++ jargon2-core-if/src/main/java/org/irods/jargon2/core/pub/io/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * Java .io implementations
+ * @author mconway
+ *
+ */
+package org.irods.jargon2.core.pub.io;
---
+++ jargon2-core-if/src/main/java/org/irods/jargon2/core/pub/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * Base package for interfaces describing new Jargon API
+ * @author mconway
+ *
+ */
+package org.irods.jargon2.core.pub;
